a4b63d9518371776d6a6058800a487169bc37d40
Juan Carlos <jcguitar1994@gmail.com>
2020-04-02 21:34:38 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(Expenses());
+
+class Expenses extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas Pessoais"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.blue,
+              child: Text("Gráfico"),            
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("Lista de Transações"),
+          ),
+        ],
+      ),
+    );
+  }
+}
