7bbe892d0927fb452512527fb5cfe14dbc36164b
Juan Carlos <jcguitar1994@gmail.com>
2020-04-02 21:44:11 -0300

    Creating data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(Expenses());
@@ -13,6 +14,21 @@ class Expenses extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+      id: "t1",
+      title: "Novo Tênis de Corrida",
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Conta de Luz",
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +42,7 @@ class MyHomePage extends StatelessWidget {
           Container(
             child: Card(
               color: Colors.blue,
-              child: Text("Gráfico"),            
+              child: Text("Gráfico"),
               elevation: 5,
             ),
           ),
