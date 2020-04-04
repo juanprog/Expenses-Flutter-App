ea98f4d02fb55bb6adb16a85822b1e57f06fec4d
Juan Carlos <jcguitar1994@gmail.com>
2020-04-03 21:31:43 -0300

    Creating a file for TransactionForm widget

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  TransactionForm({@required this.onSubmit});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: "Título",
+              ),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(
+                labelText: "Valor (R\$)",
+              ),
+            ),
+            FlatButton(
+              child: Text("Nova Transação"),
+              textColor: Colors.purple,
+              onPressed: () {
+                final title = titleController.text;
+                final value = double.tryParse(valueController.text) ?? 0.0;
+                onSubmit(title, value);
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
